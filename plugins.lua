f4b905f8156ae68706161788fa5d7f6fec91e675
Victor Rogers <victor.r.rogers@gmail.com>
2024-01-05 08:12:02 -0600

    add rust tools config

--- plugins.lua
+++ plugins.lua
@@ -57,9 +57,13 @@ local plugins = {
   {
     "simrat39/rust-tools.nvim",
     ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
     opts = function ()
       return require "custom.configs.rust-tools"
     end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
   },
 
   -- To make a plugin not be loaded
